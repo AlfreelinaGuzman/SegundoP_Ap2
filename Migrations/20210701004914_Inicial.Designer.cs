52e3e532c1b810a8b12618b7446f31aa7c9be473
AlfreelinaGuzman <alfreelinaguzman@gmail.com>
2021-06-30 20:51:58 -0400

    Commit Final

--- Migrations/20210701004914_Inicial.Designer.cs
+++ Migrations/20210701004914_Inicial.Designer.cs
@@ -9,7 +9,7 @@ using SegundoP_Ap2.DAL;
 namespace SegundoP_Ap2.Migrations
 {
     [DbContext(typeof(Contexto))]
-    [Migration("20210630233514_Inicial")]
+    [Migration("20210701004914_Inicial")]
     partial class Inicial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
